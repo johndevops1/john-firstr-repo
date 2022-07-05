fee921550826f5d242cf5aa748e5517105a77ccf
johndevops1 <john.devopsuk1@gmail.com>
2022-07-06 00:48:26 +0100

    git-actions

--- backend.tf
+++ backend.tf
@@ -1,13 +1,13 @@
 terraform {
   backend "s3" {
-    bucket = "octopus-dev-infra-terraform-state"
-    key    = "global/s3/terraform.tfstate"
-    region = "us-east-2"
+    bucket         = "octopus-dev-infra-terraform-state"
+    key            = "global/s3/terraform.tfstate"
+    region         = "us-east-2"
     dynamodb_table = "terraform-dev-state-locking"
-    encrypt = true
+    encrypt        = true
   }
 }
 
 provider "aws" {
-    region = "us-east-2"
+  region = "us-east-2"
 }
