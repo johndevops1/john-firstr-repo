70095beac2b4a126b633cb6a3cbcbebc8b3ec8ae
johndevops1 <john.devopsuk1@gmail.com>
2022-07-06 18:38:42 +0100

    git-actions

---
+++ security-groups.tf
@@ -0,0 +1,67 @@
+resource "aws_security_group" "worker_group_mgmt_one" {
+  name_prefix = "worker_group_mgmt_one"
+  vpc_id      = module.vpc.vpc_id
+
+  ingress {
+    from_port = 22
+    to_port   = 22
+    protocol  = "tcp"
+
+    cidr_blocks = [
+      "10.0.0.0/8",
+    ]
+  }
+  tags = { Project = "Octopus-${var.env}" }
+}
+
+resource "aws_security_group" "worker_group_mgmt_two" {
+  name_prefix = "worker_group_mgmt_two"
+  vpc_id      = module.vpc.vpc_id
+
+  ingress {
+    from_port = 22
+    to_port   = 22
+    protocol  = "tcp"
+
+    cidr_blocks = [
+      "192.168.0.0/16",
+    ]
+  }
+  tags = { Project = "Octopus-${var.env}" }
+}
+
+resource "aws_security_group" "all_worker_mgmt" {
+  name_prefix = "all_worker_management"
+  vpc_id      = module.vpc.vpc_id
+
+  ingress {
+    from_port = 22
+    to_port   = 22
+    protocol  = "tcp"
+
+    cidr_blocks = [
+      "10.0.0.0/8",
+      "172.16.0.0/12",
+      "192.168.0.0/16",
+    ]
+  }
+  tags = { Project = "Octopus-${var.env}" }
+}
+
+resource "aws_security_group" "rds-securitygroup" {
+  name_prefix = "rds-securitygroup-octopus"
+  vpc_id      = module.vpc.vpc_id
+
+  ingress {
+    from_port = 1433
+    to_port   = 1433
+    protocol  = "tcp"
+
+    cidr_blocks = [
+      "10.0.0.0/8",
+      "172.16.0.0/12",
+      "192.168.0.0/16",
+    ]
+  }
+  tags = { Project = "Octopus-${var.env}" }
+}
