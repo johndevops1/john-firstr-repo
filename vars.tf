d381e2ee836f146ce0b760bab5a0f00260b22bbc
johndevops1 <john.devopsuk1@gmail.com>
2022-07-06 19:30:15 +0100

    git-actions

--- vars.tf
+++ vars.tf
@@ -4,7 +4,7 @@ variable "instance_type_wg2" {}
 variable "rds_engine" {}
 variable "rds_instance_class" {}
 variable "rds_username" {}
-# variable "rds_password" {} "value fetches from github secrets"
+variable "rds_password" {}
 variable "parameter_group_name" {}
 variable "vpc_cidr" {}
 variable "private_subnets" { type = list(any) }
