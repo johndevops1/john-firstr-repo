85d28ccaadfb50c1bc4f98fe502b06ebd9dcaebf
johndevops1 <john.devopsuk1@gmail.com>
2022-07-06 21:48:59 +0100

    git-actions

--- vpc.tf
+++ vpc.tf
@@ -8,11 +8,13 @@ module "vpc" {
   source  = "terraform-aws-modules/vpc/aws"
   version = "3.2.0"
 
+  count = length(var.private_subnets)
+
   name                 = "Octopus-${var.env}"
   cidr                 = var.vpc_cidr
   azs                  = data.aws_availability_zones.available.names
-  private_subnets      = element(var.private_subnets, count.index)
-  public_subnets       = element(var.public_subnets, count.index)
+  private_subnets      = element(var.private_subnets,count.index)
+  public_subnets       = element(var.public_subnets,count.index)
   enable_nat_gateway   = true
   single_nat_gateway   = true
   enable_dns_hostnames = true
