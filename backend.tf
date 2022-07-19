b330b999c6b9e3f5b222eb11cf057269298e3bc1
johndevops1 <john.boothapati@sage.com>
2022-07-19 09:46:01 +0100

    git

--- backend.tf
+++ backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket = "octopus-infra-terraform-state"
+    bucket = "octopus-dev-infra-terraform-state"
     key    = "global/s3-workspace-test/terraform.tfstate"
     region = "eu-west-1"
     # dynamodb_table = "terraform-dev-state-locking"
